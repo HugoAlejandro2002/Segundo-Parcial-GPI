422f9a1b5a10353f13b4ade8e52cbf7e0386b154
Marcos Simon <marcosht503@gmail.com>
2023-12-10 20:07:37 -0400

    Fase de planificacion

--- src/pages/FasePlanificacion.jsx
+++ src/pages/FasePlanificacion.jsx
@@ -1,9 +1,150 @@
 
+
 function FasePlanificacion() {
   return (
-    <div>
-      Fase de Planificacion
-    </div>
+
+        <div className="container mx-auto px-10 py-8 rounded-3xl bg-slate-50" style={{ backgroundColor: 'rgba(217, 217, 217, 0.74)' }}>
+            <h1 className='text-2xl font-bold' style={{ color: '#00377B' }}>Fase de Planificación </h1>
+            <p className="text-xl font-light text-left py-4 " style={{ color: '#717171' }}>
+                En la fase de planificación, se establece un cronograma detallado que abarca todas las etapas del
+                desarrollo del
+                sistema web. Este cronograma incluye:
+            </p>
+
+            <ul className="list-disc pl-6 text-xl font-light pb-4" style={{ color: '#717171' }}>
+                <li>Fase de Inicio: Definición de objetivos, análisis de requisitos y asignación de recursos.</li>
+                <li>Fase de Diseño: Elaboración de wireframes y mockups en Figma, con revisiones y ajustes periódicos.
+                </li>
+                <li>Fase de Desarrollo: Implementación del front-end usando React.js y desarrollo del back-end con
+                    Express.js,
+                    siguiendo un enfoque iterativo.
+                </li>
+                <li>Fase de Pruebas: Realización de pruebas unitarias y de integración utilizando Jest, Cypress y Mocha,
+                    asegurando la calidad y la funcionalidad del sitio.
+                </li>
+                <li>Fase de Implementación: Despliegue del sitio web y su puesta en marcha.</li>
+                <li>Fase de Cierre: Evaluación del proyecto, documentación y transición a operaciones.</li>
+            </ul>
+
+
+            <div className="text-center mb-9" style={{ color: '#00377B' }}>
+
+                <a href="https://www.notion.so/8d7c36374b024d45b6c7d54f73acb7ec?v=fe088133a4f2405486be9f65d05afca4" target="_blank" rel = "noreferrer">
+                    <h2 className="text-2xl font-bold text-left text-primary uppercase py-4">Plan General del
+                        Proyecto</h2>
+                </a>
+
+                <a href="https://www.notion.so/7fd2db96e764452388165ceff6d6308a?v=5aedbce3e42947b1b8a30b282959288d" target="_blank" rel = "noreferrer">
+                <h2 className="text-2xl font-bold text-left text-primary uppercase py-4">Plan General Web
+                    Backend</h2>
+                </a>
+
+                <a href="https://www.notion.so/c7620716a92a4c7dbe088a606a6dc196?v=0664661cbb974175a67e8bc50e26f44a" target="_blank" rel = "noreferrer">
+                <h2 className="text-2xl font-bold text-left text-primary uppercase py-4">Plan General Web
+                    Frontend</h2>
+                </a>
+
+            </div>
+
+
+            <h1 className='text-2xl font-bold' style={{ color: '#00377B' }}>Uso de herramientas </h1>
+
+            <p className="text-xl font-light text-left py-4" style={{ color: '#717171' }}>
+                Para la gestión del proyecto, se utilizan diversas herramientas especializadas:
+            </p>
+
+            <ul className="list-disc pl-6 text-xl font-light pb-4" style={{ color: '#717171' }}>
+                <li>Planificación y Seguimiento: Uso de Notion para la gestión de tareas, seguimiento del progreso y
+                    documentación del proyecto.
+                </li>
+                <li>Diseño y Prototipado: Figma para el diseño de wireframes y mockups, facilitando la colaboración y el
+                    feedback.
+                </li>
+                <li>Desarrollo: Vue.js para el front-end y Express.js para el back-end, proporcionando un marco sólido
+                    para el
+                    desarrollo ágil.
+                </li>
+                <li>Pruebas: Jest, Cypress y Mocha para pruebas unitarias y de integración, garantizando la calidad y el
+                    rendimiento del sitio web.
+                </li>
+            </ul>
+
+            <br/>
+            <div className="grid grid-cols-3 gap-4 justify-center items-center" style={{ color: '#717171' }}>
+                <div className="text-center">
+                    <img src="https://th.bing.com/th/id/R.b6b42b0d08c8685ae401ce792a9b58fc?rik=57IEh7EFsxXmRA&pid=ImgRaw&r=0" alt="Logo de Notion" className="w-20 h-20 mx-auto"/>
+                    <p>Notion</p>
+                </div>
+                <div className="text-center">
+                    <img src="https://th.bing.com/th/id/OIP.akV9KjAtdaROFei8n8dnowHaHw?rs=1&pid=ImgDetMain"
+                         alt="Logo de Figma"
+                         className="w-20 h-20 mx-auto"/>
+                        <p>Figma</p>
+                </div>
+                <div className="text-center">
+                    <img
+                        src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"
+                        alt="Logo de React.js"
+                        className="w-20 h-20 mx-auto"/>
+                        <p>React.js</p>
+                </div>
+                <div className="text-center">
+                    <img src="https://riseuplabs.com/wp-content/uploads/2021/04/express.js-icon.png"
+                         alt="Logo de Express.js"
+                         className="w-20 h-20 mx-auto"/>
+                        <p>Express.js</p>
+                </div>
+                <div className="text-center">
+                    <img
+                        src="https://th.bing.com/th/id/R.cfa3a32613483a1ee48fac169aeb6359?rik=Lxp1uM0Qxn40kA&pid=ImgRaw&r=0"
+                        alt="Logo de Jest" className="w-20 h-20 mx-auto"/>
+                        <p>Jest</p>
+                </div>
+                <div className="text-center">
+                    <img
+                        src="https://th.bing.com/th/id/R.a89c871423e0845750d27cf4ea910e53?rik=JvWxlJgKBtnUyQ&pid=ImgRaw&r=0"
+                        alt="Logo de Cypress"
+                        className="w-20 h-20 mx-auto"/>
+                        <p>Cypress</p>
+                </div>
+                <div className="text-center">
+                    <img
+                        src="https://th.bing.com/th/id/R.55b112b344a5cb45a6becf64bf5a5cf2?rik=sAaXelEMUzQDrw&pid=ImgRaw&r=0"
+                        alt="Logo de Mocha JS"
+                        className="w-20 h-20 mx-auto"/>
+
+                        <p>Mocha JS</p>
+                </div>
+            </div>
+            <br/>
+
+            <a href="https://docs.google.com/document/d/1KGTHxPwsybFohuLlVXKgVeSX7r8Akobdpf8Fc6xkzxc/edit" target="_blank" rel = "noreferrer">
+                <h1 className='text-2xl font-bold' style={{ color: '#00377B' }}>Gestión de Riesgos</h1>
+            </a>
+            <br/>
+
+            <ul className="list-disc pl-6 text-xl font-light pb-4" style={{ color: '#717171' }}>
+                <li>Identificación de Riesgos: Riesgos técnicos, de recursos y de tiempo evaluados en impacto y
+                    probabilidad.
+                </li>
+                <li>Análisis y Evaluación: Uso de una escala del 1 al 5 para evaluar probabilidad e impacto de cada
+                    riesgo.
+                </li>
+                <li>Estrategias de Mitigación: Desarrollo de planes de acción para cada riesgo identificado.</li>
+                <li>Plan de Contingencia: Medidas para enfrentar los riesgos, como tener equipos de desarrollo de
+                    respaldo.
+                </li>
+                <li>Monitoreo y Comunicación: Revisión periódica de los riesgos y comunicación constante con el equipo y
+                    los
+                    stakeholders.
+                </li>
+
+                <li>
+                    Riesgos Identificados y Estrategias de Mitigación: Lista de todos los riesgos identificados, junto a
+                    las estrategias necesarias para su mitigación.
+                </li>
+            </ul>
+        </div>
   );
 }
 
