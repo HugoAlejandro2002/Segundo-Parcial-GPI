66ea770203acee43e3a3beeafcff4d77f3477530
Dylan-Chambi <dylanchambifi@gmail.com>
2023-12-12 11:06:49 -0400

    Changing homepage on build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://hugoalejandro2002.github.io/Segundo-Parcial-GPI/"
 })
