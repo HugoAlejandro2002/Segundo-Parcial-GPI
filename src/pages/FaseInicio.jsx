94c0ea978c92d0fedcd48e5ca1c89c82fe435862
HugoAlejandro2002 <apazahuaychohugoalejandro@gmail.com>
2023-12-11 21:53:12 -0400

    feat: fase inicio y fase cierre

--- src/pages/FaseInicio.jsx
+++ src/pages/FaseInicio.jsx
@@ -1,23 +1,53 @@
-import { useState } from 'react';
-import reactLogo from '../assets/react.svg';
-import Button from '@mui/material/Button';
+import pez_diagram from '../assets/pez_diagram.png';
+
 
 function FaseInicio() {
-  const [count, setCount] = useState(0);
+
 
   return (
-    <div className="bg-gray-100 h-screen flex flex-col items-center justify-center">
-      <img src={reactLogo} alt="React Logo" className="w-32 h-32" />
-      <h1 className="text-2xl font-bold mt-4">¡Hola, mundo!</h1>
-      <p className="text-gray-600">Esta es una aplicación de ejemplo con Tailwind CSS y Material-UI.</p>
-      <Button
-        variant="contained"
-        color="primary"
-        className="mt-4"
-        onClick={() => setCount(count + 1)}
-      >
-        Contador: {count}
-      </Button>
+    <div className="container mx-auto px-10 py-8 rounded-3xl bg-slate-50" style={{ backgroundColor: 'rgba(217, 217, 217, 0.74)' }}>
+      <h1 className='text-2xl font-bold' style={{ color: '#00377B' }}>Fase de Inicio del Proyecto CRM para TIGO</h1>
+
+      <p className="text-xl font-light text-left py-4 " style={{ color: '#717171' }}>
+        <strong>Problema Identificado:</strong> TIGO enfrentaba desafíos clave como la falta de un chatbot eficiente, métodos ineficaces de recolección de información, seguimiento de clientes deficiente, comunicación limitada con los clientes, y una estrategia de marketing interna y externa insuficiente.
+      </p>
+
+      
+      <div className='flex justify-center'>
+        <img src={pez_diagram} alt="Gartner Cloud" className='w-1/2' />
+      </div>
+
+      <a href="https://docs.google.com/document/d/1lnXYpDph5zg_IzsjkkpAH6iaiOoOhlxW78aKPsb8hqU" target="_blank" rel="noreferrer">
+        <h2 className="text-2xl font-bold text-left text-primary uppercase py-4" style={{ color: '#00377B' }}>Ishikawa</h2>
+      </a>
+
+      <p className="text-xl font-light text-left py-4 " style={{ color: '#717171' }}>
+        <strong>Objetivo General:</strong> Desarrollar un sistema CRM completo para mejorar la fidelización de clientes y fortalecer la comunicación integral con ellos a través de soluciones tecnológicas avanzadas.
+      </p>
+      <h2 className='text-xl font-bold' style={{ color: '#00377B' }}>Límites y Alcances</h2>
+      <ul className="list-disc pl-6 text-xl font-light pb-4" style={{ color: '#717171' }}>
+        <li><strong>Límites:</strong> El sistema no incluye soporte de proveedores externos, soporte técnico posentrega, ni considera la infraestructura actual de las instalaciones proporcionada por TIGO.</li>
+        <li><strong>Alcances:</strong> Implementación del CRM en todas las regiones de operación de TIGO, enfocándose en asegurar funcionalidad y accesibilidad, incluso en áreas con conectividad limitada. El sistema se adaptará a una variedad de entornos y dispositivos para maximizar la eficiencia y efectividad.</li>
+      </ul>
+
+      <a href="https://docs.google.com/document/d/1kkkwFm4oVtM7q4DWBURxmzV4wEk5oB_qlLzK4KUayIE/edit" target="_blank" rel="noreferrer">
+        <h2 className="text-2xl font-bold text-left text-primary uppercase py-4" style={{ color: '#00377B' }}>Limites y Alcance</h2>
+      </a>
+
+      <h2 className='text-xl font-bold' style={{ color: '#00377B' }}>Estrategia de Implementación</h2>
+      <ul className="list-disc pl-6 text-xl font-light pb-4" style={{ color: '#717171' }}>
+        <li><strong>Desarrollo de la Página Web y CRM:</strong> Incluye diseño, desarrollo, pruebas, y mantenimiento, con una fuerte integración de un chatbot para la comunicación con los usuarios.</li>
+        <li><strong>Soporte y Mantenimiento:</strong> Corrección de errores, actualizaciones basadas en análisis de datos y tendencias tecnológicas, y asistencia técnica continua.</li>
+        <li><strong>Capacitación y Transferencia de Conocimientos:</strong> Sesiones de capacitación para el personal de TIGO y desarrollo de manuales detallados.</li>
+        <li><strong>Integración y Seguridad:</strong> Enfocada en la integración efectiva con herramientas de terceros y la implementación de robustas medidas de seguridad.</li>
+      </ul>
+
+      <a href="https://docs.google.com/document/d/1xPzpSPTthH6z1mFiel6cKxumysJxxQrbc8tfWcW7Es4/edit" target="_blank" rel="noreferrer">
+        <h2 className="text-2xl font-bold text-left text-primary uppercase py-4" style={{ color: '#00377B' }}>TDR</h2>
+      </a>
+      <a href="https://docs.google.com/document/d/1-obTmJ7r2aVal62OtEyIlBzfNBhH70iNCGl0mtuB88Y/edit" target="_blank" rel="noreferrer">
+        <h2 className="text-2xl font-bold text-left text-primary uppercase py-4" style={{ color: '#00377B' }}>Carta de Inicio</h2>
+      </a>
     </div>
   );
 }
